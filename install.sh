5bcdf9e5154b94d5bf192166d2bb22261f54d6c0
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-06-04 13:27:55 +0530

    Running the installation if the file is not a link or the link is broken
    Enabling debug + error mode in bash

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -e -x
+
 # Install go toolchain
 GO_BINARY="/usr/bin/go"
 GO_VERSION="${GO_VERSION:-1.6}"
 GO_BASE_PATH="/usr/local"
 GOROOT="/usr/local/go"
-if [ ! -f ${GO_BINARY} ]; then
+if [ ! -L ${GO_BINARY} ] || [ ! -e ${GO_BINARY} ]; then
   rm -rf ${GOROOT}
   curl -sSL https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz | tar -xzf - -C ${GO_BASE_PATH}
   for BINARY in `ls ${GO_BASE_PATH}/go/bin`
@@ -16,7 +18,7 @@ fi
 
 # Install Glide dependency mgmt tool
 GLIDE_BINARY="/usr/bin/glide"
-if [ ! -f ${GLIDE_BINARY} ]; then
+if [ ! -L ${GLIDE_BINARY} ] || [ ! -e ${GLIDE_BINARY} ]; then
   curl -sSL https://github.com/Masterminds/glide/releases/download/0.9.1/glide-0.9.1-linux-amd64.tar.gz | tar -xzf - -C /tmp/
   chmod +x /tmp/linux-amd64/glide
   mv /tmp/linux-amd64/glide /usr/bin/
